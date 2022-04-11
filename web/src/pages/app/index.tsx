95b6aa79b2a1b88d955d6c080919e7bf38163625
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-04-11 15:50:41 -0300

    [Web] WithApollo

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,18 +1,32 @@
-import { getAccessToken, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { gql, useQuery } from "@apollo/client";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { GetServerSideProps, NextPage } from "next";
+import { withApollo } from "../../lib/withApollo";
+
+const PRODUCTS_QUERY = gql`
+  query GetProducts {
+    products {
+      id
+      title
+    }
+  }
+`;
 
 const Home: NextPage = () => {
+  const { user } = useUser();
+  const { data, loading, error } = useQuery(PRODUCTS_QUERY);
+
+  console.log(data);
+
   return <h1>Hello world</h1>;
 };
 
-export default Home;
-
 export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
   getServerSideProps: async ({ req, res }) => {
-    console.log(getAccessToken(req, res));
-
     return {
       props: {},
     };
   },
 });
+
+export default withApollo(Home);
