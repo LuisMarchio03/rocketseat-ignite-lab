e084046c94e7b7d69c18226e4bde6b01434b45f2
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-04-12 09:50:55 -0300

    [Web] Codegen

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -3,20 +3,8 @@ import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { GetServerSideProps, NextPage } from "next";
 import { withApollo } from "../../lib/withApollo";
 
-const PRODUCTS_QUERY = gql`
-  query GetProducts {
-    products {
-      id
-      title
-    }
-  }
-`;
-
 const Home: NextPage = () => {
   const { user } = useUser();
-  const { data, loading, error } = useQuery(PRODUCTS_QUERY);
-
-  console.log(data);
 
   return <h1>Hello world</h1>;
 };
